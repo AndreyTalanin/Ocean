6f256008b0ee6297733dc17e8080005311d7fe1f
Andrey Talanin <talanin.aa.nnov@gmail.com>
2020-05-08 23:23:04 +0300

    Edit the OceanGame class so now it represents a complete application

--- Source/Ocean/OceanGame.cs
+++ Source/Ocean/OceanGame.cs
@@ -17,11 +17,20 @@ namespace Ocean
         private const int c_windowWidth = 1280;
         private const int c_windowHeight = 720;
         private const int c_targetFps = 25;
+        private const int c_oceanWidth = c_windowWidth / 4;
+        private const int c_oceanHeight = c_windowHeight / 4;
+
+        private Ocean m_ocean;
+        private OceanRenderer m_oceanRenderer;
+        private bool m_disposed;
 
         protected GraphicsDeviceManager GraphicsDeviceManager { get; }
 
         public OceanGame()
         {
+            m_ocean = null;
+            m_oceanRenderer = null;
+            m_disposed = false;
             GraphicsDeviceManager = new GraphicsDeviceManager(this)
             {
                 IsFullScreen = c_isFullScreen,
@@ -34,10 +43,19 @@ namespace Ocean
             TargetElapsedTime = TimeSpan.FromSeconds(1) / c_targetFps;
         }
 
+        protected override void LoadContent()
+        {
+            m_ocean = new Ocean(c_oceanWidth, c_oceanHeight);
+            m_oceanRenderer = new OceanRenderer(c_windowWidth, c_windowHeight, m_ocean, GraphicsDevice);
+            base.LoadContent();
+        }
+
         protected override void Update(GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            m_ocean.Update();
 
             base.Update(gameTime);
         }
@@ -46,7 +64,25 @@ namespace Ocean
         {
             GraphicsDevice.Clear(Color.Black);
 
+            m_oceanRenderer.Draw();
+
             base.Draw(gameTime);
+        }
+
+        protected override void Dispose(bool disposeManagedResources)
+        {
+            if (!m_disposed && disposeManagedResources)
+            {
+                if (disposeManagedResources)
+                {
+                    m_ocean?.Dispose();
+                    m_oceanRenderer?.Dispose();
+                }
+
+                m_disposed = true;
+            }
+
+            base.Dispose(disposeManagedResources);
         }
     }
 }
