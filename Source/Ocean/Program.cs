feb987925107a419f2cb853c25945ed3af48c42d
Andrey Talanin <talanin.aa.nnov@gmail.com>
2020-05-08 19:27:59 +0300

    Create a MonoGame application project

---
+++ Source/Ocean/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ocean - Copyright © Andrey Talanin 2020
+// This file is subject to the terms and conditions defined in the
+// file 'LICENSE.md', which is a part of this source code package.
+
+using System;
+
+namespace Ocean
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (OceanGame game = new OceanGame())
+                game.Run();
+        }
+    }
+}
